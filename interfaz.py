ee215e56656d5e3dbc5e383848ced998a38b8bce
JaviMunir <moubayyed47@hotmail.com>
2025-01-25 18:15:16 +0100

    💄 add interface for main program

--- interfaz.py
+++ interfaz.py
@@ -8,15 +8,15 @@ def clear_screen():
 def show_logo():
     """Display the logo at the top of the interface."""
     logo = r'''
-    88b           d88  88888888888  ,ad8888ba,    ,ad8888ba,    88888888888  88           88  8b        d8  
-    888b         d888  88          d8"'    `"8b  d8"'    `"8b   88           88           88   Y8,    ,8P   
-    88`8b       d8'88  88         d8'           d8'        `8b  88           88           88    `8b  d8'    
-    88 `8b     d8' 88  88aaaaa    88            88          88  88aaaaa      88           88      Y88P      
-    88  `8b   d8'  88  88"""""    88            88          88  88"""""      88           88      d88b      
-    88   `8b d8'   88  88         Y8,           Y8,        ,8P  88           88           88    ,8P  Y8,    
-    88    `888'    88  88          Y8a.    .a8P  Y8a.    .a8P   88           88           88   d8'    `8b   
+    88b           d88  88888888888  ,ad8888ba,    ,ad8888ba,    88888888888  88           88  8b        d8
+    888b         d888  88          d8"'    `"8b  d8"'    `"8b   88           88           88   Y8,    ,8P
+    88`8b       d8'88  88         d8'           d8'        `8b  88           88           88    `8b  d8'
+    88 `8b     d8' 88  88aaaaa    88            88          88  88aaaaa      88           88      Y88P
+    88  `8b   d8'  88  88"""""    88            88          88  88"""""      88           88      d88b
+    88   `8b d8'   88  88         Y8,           Y8,        ,8P  88           88           88    ,8P  Y8,
+    88    `888'    88  88          Y8a.    .a8P  Y8a.    .a8P   88           88           88   d8'    `8b
     88     `8'     88  88888888888  `"Y8888Y"'    `"Y8888Y"'    88           88888888888  88  8P        Y8
-    
+
                                             by: K1loWatt
     '''
     print(logo)
@@ -46,10 +46,10 @@ def main():
         clear_screen()
         show_logo()
         menu()
-        
+
         # Get user input
         choice = input("\nEnter your choice: ")
-        
+
         if choice == "1":
             option_one()
         elif choice == "2":
@@ -64,4 +64,3 @@ def main():
 # Run the interface
 if __name__ == "__main__":
     main()
-
